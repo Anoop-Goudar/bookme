b522c57f537cfe624256dbb6050d1dbb34f7d373
Anoop Goudar <anoop.a.goudar@gmail.com>
2020-05-16 21:34:11 +0530

    added a sample registration page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+
+import Signup from "./signup";
 
 function App() {
   return (
     <div className="App">
+      <HashRouter>
+          <ul className="header">
+            <li><a href="/">Home</a></li>
+            <li><NavLink to="/signup">Sign Up</NavLink></li>
+          </ul>
+          <div className="content">
+            {/* <Route path="/" component={}/> */}
+            <Route path="/signup" component={Signup}/>
+          </div>
+        </HashRouter>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
